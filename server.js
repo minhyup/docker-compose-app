878cb437499d0291fdfba6ed8797ac09c7532e52
kang <minhyupp@gmail.com>
2020-09-29 05:05:35 +0900

    'fist'

---
+++ server.js
@@ -0,0 +1,27 @@
+console.log("node!");
+
+const express = require("express");
+const redis = require("redis");
+
+// 레디스 클라이언트 생성
+const redisClient = redis.createClient({
+  host: "redis-server",
+  port: 6379,
+});
+
+const app = express();
+
+// 숫자는 0부터 시작합니다.
+redisClient.set("number", 0);
+
+app.get("/", (req, res) => {
+  redisClient("number", (err, number) => {
+    // 현재 숫자를 가져운 후에 1씩 올려준다.
+    redisClient.set("number", parseInt(number) + 1);
+    res.send("숫자가 1씩 올라갑니다. 숫자:" + number);
+  });
+});
+
+app.listen(8080);
+
+console.log("Server is running");
